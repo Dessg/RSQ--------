6ba74a2e17c50522dc2901bf82abd032204df35d
Anton Stalmakov <126509811+TonySW337@users.noreply.github.com>
2023-04-01 12:57:15 +0300

    patch

--- script.js
+++ script.js
@@ -21,34 +21,6 @@ function random_button() {
     let number = Math.floor(Math.random() * 124 + 1);
     document.getElementById("result_random").innerHTML = "ROLL(1-124):" + number + " " + heroes_pull[number];
 }
-// vvv  здесь тестовая функция vvv
-let data_stat =  null;
-
-const getStatistic = async () => {
-    await fetch("https://api.opendota.com/api/players/125325708")
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Network response was not ok");
-        }
-      })
-      .then((data) => {
-        data_stat = data;
-        console.log(data_stat);
-        document.getElementById("stats_test").innerHTML = data_stat.profile.personaname;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  //  ^^^  здесь тестовая функция ^^^
-  
-  //здесь будут функции по выводу побед / поражений  
-  let wl1,wl2,wl3,wl4,wl5,wl6,wl7 = null;
-  let wins_1,wins_2,wins_3,wins_4,wins_5,wins_6,wins_7 = null;
-  let loss_1,loss_2,loss_3,loss_4,loss_5,loss_6,loss_7 = null;
 
   /* id аккаунтов 
   sti - 1196955551
@@ -60,34 +32,36 @@ const getStatistic = async () => {
   Poni - 323474403
   */
  
-  async function getWLStats() {
+  async function getWLStats(id) {
     try {
-      const response_1 = await fetch("https://api.opendota.com/api/players/1196955551/wl");
-      const response_2 = await fetch("https://api.opendota.com/api/players/125325708/wl");
-      const response_3 = await fetch("https://api.opendota.com/api/players/167874436/wl");
-      const response_4 = await fetch("https://api.opendota.com/api/players/192089161/wl");
-      const response_5 = await fetch("https://api.opendota.com/api/players/226799449/wl");
-      const response_6 = await fetch("https://api.opendota.com/api/players/1200198064/wl");
-      const response_7 = await fetch("https://api.opendota.com/api/players/323474403/wl");
+      let nick = null;
+      switch (id){
+        case 1196955551:
+          nick = "STI";
+          break;
+        case 125325708:
+          nick = "Tony SW337";
+          break;
+        case 167874436:
+          nick = "rodyaN";
+          break;
+        case 192089161:
+          nick = "Mast1v";
+          break;
+        case 226799449:
+          nick = "Atom";
+          break;
+        case 1200198064:
+          nick = "dragon rage";
+          break;
+        case 323474403:
+          nick = "Пони";
+          break;
+      }
+      let response = await fetch("https://api.opendota.com/api/players/"+ id +"/wl");
+      let wl = await response.json();
       
-
-      wl1 = await response_1.json();
-      wl2 = await response_2.json();
-      wl3 = await response_3.json();
-      wl4 = await response_4.json();
-      wl5 = await response_5.json();
-      wl6 = await response_6.json();
-      wl7 = await response_7.json();
-
-      // console.log(wins_1 , wins_2 , wins_3 , wins_4, wins_5, wins_6, wins_7) ;
-      document.getElementById("stats_test1").innerHTML =  "STI " + "Побед:" +wl1.win + " /Поражений :" + wl1.lose + " /WR: " + ((wl1.win/(wl1.win+wl1.lose))*100);
-      document.getElementById("stats_test2").innerHTML =  "Tony " + "Побед:" +wl2.win + " /Поражений :" + wl2.lose + " /WR: " + ((wl2.win/(wl2.win+wl2.lose))*100);
-      document.getElementById("stats_test3").innerHTML =  "Rondo " + "Побед:" +wl3.win + " /Поражений :" + wl3.lose + " /WR: " + ((wl3.win/(wl3.win+wl3.lose))*100);
-      document.getElementById("stats_test4").innerHTML =  "Mast1v " + "Побед:" +wl4.win + " /Поражений :" + wl4.lose + " /WR: " + ((wl4.win/(wl4.win+wl4.lose))*100);
-      document.getElementById("stats_test5").innerHTML =  "Atom " + "Побед:" +wl5.win + " /Поражений :" + wl5.lose + " /WR: " + ((wl5.win/(wl5.win+wl5.lose))*100);
-      document.getElementById("stats_test6").innerHTML =  "dragon rage " + "Побед:" +wl6.win + " /Поражений :" + wl6.lose + " /WR: " + ((wl6.win/(wl6.win+wl6.lose))*100);
-      document.getElementById("stats_test7").innerHTML =  "Пони " + "Побед:" +wl7.win + " /Поражений :" + wl7.lose + " /WR: " + ((wl7.win/(wl7.win+wl7.lose))*100);
-
+      document.getElementById(id).innerHTML =  nick + ": Побед:" + wl.win + " /Поражений :" + wl.lose + " /WR: " + Math.round((wl.win/(wl.win+wl.lose))*100) + "%";
     } catch(error) {
       console.log("error ,try again");
     }
